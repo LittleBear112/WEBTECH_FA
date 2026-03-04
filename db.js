b5b97980505a0428f2fcdcd2e3060a9eff530e71
LittleBear112 <nevanleewen112@gmail.com>
2026-03-04 22:27:48 +0800

    connect to aiven database

--- db.js
+++ db.js
@@ -2,15 +2,13 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-// REMOVE db.connect()
 
 module.exports = db;
