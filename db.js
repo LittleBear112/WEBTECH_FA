e333c966c578ace813ff6dd5792c9c3c972d0fdb
LittleBear112 <nevanleewen112@gmail.com>
2026-03-05 00:12:22 +0800

    use aiven service uri

--- db.js
+++ db.js
@@ -1,20 +1,10 @@
 const mysql = require("mysql2");
-const fs = require("fs");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    ca: fs.readFileSync("./ca.pem")
-  }
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
   if (err) {
-    console.error("DB connection error:", err);
+    console.error("Database connection failed:", err);
   } else {
     console.log("Database connected");
   }
